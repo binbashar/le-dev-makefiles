91c349b985ce562898ff6fea2295a82e1b54dcec
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2020-10-05 11:58:28 -0300

    BBL-381 | updating mkdocs-material image version

--- mkdocs/mkdocs-material.mk
+++ mkdocs/mkdocs-material.mk
@@ -3,7 +3,7 @@ SHELL                    := /bin/bash
 
 LOCAL_OS_USER_ID         := $(shell id -u)
 LOCAL_OS_GROUP_ID        := $(shell id -g)
-MKDOCS_DOCKER_IMG        := squidfunk/mkdocs-material:6.0.1
+MKDOCS_DOCKER_IMG        := squidfunk/mkdocs-material:6.0.2
 
 help:
 	@echo 'Available Commands:'
