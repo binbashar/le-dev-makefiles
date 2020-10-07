e7c0d8d9e32daeabb51d819344b369e1f583de4d
Diego OJeda (BinBash) <38356409+diego-ojeda-binbash@users.noreply.github.com>
2020-10-07 12:13:51 -0300

    Change some of the variables in Release Mgmt makefile to use lazy set instead of immediate set in order to avoid commands to be run at variable definition time (#1)
    Co-authored-by: Diego OJ <OJ@MacBook-Pro-de-MACBOOK.local>

--- release-mgmt/release.mk
+++ release-mgmt/release.mk
@@ -19,9 +19,9 @@ docker run --rm \
 -it binbash/git-release:0.0.2
 endef
 
-GIT_SEMTAG_VER_PATCH := $(shell ${GIT_SEMTAG_CMD_PREFIX} final -s patch -o | grep -v 'Warning: Permanently added the RSA host key for IP address')
-GIT_SEMTAG_VER_MINOR := $(shell ${GIT_SEMTAG_CMD_PREFIX} final -s minor -o | grep -v 'Warning: Permanently added the RSA host key for IP address')
-GIT_SEMTAG_VER_MAJOR := $(shell ${GIT_SEMTAG_CMD_PREFIX} final -s major -o | grep -v 'Warning: Permanently added the RSA host key for IP address')
+GIT_SEMTAG_VER_PATCH = $(shell ${GIT_SEMTAG_CMD_PREFIX} final -s patch -o | grep -v 'Warning: Permanently added the RSA host key for IP address')
+GIT_SEMTAG_VER_MINOR = $(shell ${GIT_SEMTAG_CMD_PREFIX} final -s minor -o | grep -v 'Warning: Permanently added the RSA host key for IP address')
+GIT_SEMTAG_VER_MAJOR = $(shell ${GIT_SEMTAG_CMD_PREFIX} final -s major -o | grep -v 'Warning: Permanently added the RSA host key for IP address')
 
 help:
 	@echo 'Available Commands:'
