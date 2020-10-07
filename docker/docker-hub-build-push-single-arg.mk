f9b07b48a18bce96c3a766ced6b5fcba38415a3f
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2020-10-07 09:42:17 -0300

    BBL-313 | renaming arg var at docker-hub-build-push-single-arg.mk

--- docker/docker-hub-build-push-single-arg.mk
+++ docker/docker-hub-build-push-single-arg.mk
@@ -9,10 +9,10 @@ help:
 # DOCKER-COMPOSE                                               #
 #==============================================================#
 build: ## build docker image
-	docker build -t binbash/${DOCKER_IMG_NAME}:${DOCKER_TAG} --build-arg ARG_DOCKER_TAG_VER='${DOCKER_TAG}' .
+	docker build -t binbash/${DOCKER_IMG_NAME}:${DOCKER_TAG} --build-arg DOCKER_TAG='${DOCKER_TAG}' .
 
 build-no-cache: ## build docker image no cache
-	docker build --no-cache -t binbash/${DOCKER_IMG_NAME}:${DOCKER_TAG} --build-arg ARG_DOCKER_TAG_VER='${DOCKER_TAG}' .
+	docker build --no-cache -t binbash/${DOCKER_IMG_NAME}:${DOCKER_TAG} --build-arg DOCKER_TAG='${DOCKER_TAG}' .
 
 push: ## push docker image to registry
 	docker push binbash/${DOCKER_IMG_NAME}:${DOCKER_TAG}
