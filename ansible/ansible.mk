e94c3b7c778e5845ea243d16e4e9bb111187e958
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2020-11-11 15:02:32 -0300

    BBL-242 | encrypt cmd post chown added

--- ansible/ansible.mk
+++ ansible/ansible.mk
@@ -44,9 +44,6 @@ endef
 # ANSIBLE VARS
 ANSIBLE_TAGS = $$(echo $@ | cut -d "-" -f 2- | sed "s/%*$$//")
 
-PY_PIP_VER := 20.2.4
-PY_ANSIBLE_VER := 2.10.1
-
 help:
 	@echo 'Available Commands:'
 	@egrep '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":"}; { if ($$3 == "") { printf " - \033[36m%-18s\033[0m %s\n", $$1, $$2 } else { printf " - \033[36m%-18s\033[0m %s\n", $$2, $$3 }}'
@@ -82,6 +79,7 @@ decrypt-cmd:
 decrypt-string: ## Decrypt encrypted string via ansible-vault - e.g. make ARG="your_encrypted_srting" decrypt-string
 	${ANSIBLE_CMD_PREFIX} bash ../@bin/scripts/ansible-vault-decrypt-str.sh ${ARG}
 
-encrypt: ## Encrypt secrets.dec.tf via ansible-vault
+encrypt: encrypt-cmd pwd-dir-chmod ## Encrypt secrets.dec.tf via ansible-vault
+encrypt-cmd:
 	${ANSIBLE_CMD_PREFIX} ansible-vault encrypt --output ./group_vars/secrets.enc.yml ./group_vars/secrets.dec.yml \
 	&& rm -rf ./group_vars/secrets.dec.yml
