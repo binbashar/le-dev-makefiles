159a687846f58adab7930bbda75f6c9c818ab8ad
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2021-01-05 10:16:33 -0300

    BBL-192 | upgrading to latest material mkdocs ver

--- mkdocs/mkdocs-material.mk
+++ mkdocs/mkdocs-material.mk
@@ -5,7 +5,7 @@ SHELL                    := /bin/bash
 
 LOCAL_OS_USER_ID         = $(shell id -u)
 LOCAL_OS_GROUP_ID        = $(shell id -g)
-MKDOCS_DOCKER_IMG        := squidfunk/mkdocs-material:6.1.6
+MKDOCS_DOCKER_IMG        := squidfunk/mkdocs-material:6.2.3
 
 help:
 	@echo 'Available Commands:'
