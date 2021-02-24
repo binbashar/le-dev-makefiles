b8e4421910b005fae81081480be81012e28b645f
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2021-02-24 14:56:55 -0300

    BBL-192 | updating material mkdocs ver in makefile

--- mkdocs/mkdocs-material.mk
+++ mkdocs/mkdocs-material.mk
@@ -5,7 +5,7 @@ SHELL                    := /bin/bash
 
 LOCAL_OS_USER_ID         = $(shell id -u)
 LOCAL_OS_GROUP_ID        = $(shell id -g)
-MKDOCS_DOCKER_IMG        := squidfunk/mkdocs-material:7.0.0
+MKDOCS_DOCKER_IMG        := squidfunk/mkdocs-material:7.0.1
 
 help:
 	@echo 'Available Commands:'
