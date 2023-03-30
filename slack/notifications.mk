61c5dd565946219c55f8edebb909ab0aa0e6222c
Juan Matias Kungfoo de la Camara Beovide <juan.delacamara@binbash.com.ar>
2023-03-30 12:24:18 -0300

    ISSUE-47 le-dev-tools added title

--- slack/notifications.mk
+++ slack/notifications.mk
@@ -10,8 +10,8 @@ help:
 #==============================================================#
 # NOTIFICATIONS												   #
 #==============================================================#
-notify-success: ## Sends notification on success, MESSAGE and SLACK_URL have to be set
-	@echo "curl -s -X POST -H 'Content-type: application/json' -d '{\"blocks\":[ {\"type\":\"header\", \"text\":{ \"type\":\"plain_text\", \"text\": \"Task success\" }}, {\"type\":\"divider\"}, {\"type\":\"context\", \"elements\":[ {\"type\":\"mrkdwn\", \"text\": \":white_check_mark: $(MESSAGE)\n.\" } ] } ] }' $(SLACK_URL)"
+notify-success: ## Sends notification on success, TITLE, MESSAGE and SLACK_URL have to be set
+	@echo "curl -s -X POST -H 'Content-type: application/json' -d '{\"blocks\":[ {\"type\":\"header\", \"text\":{ \"type\":\"plain_text\", \"text\": \"$(TITLE)\" }}, {\"type\":\"divider\"}, {\"type\":\"context\", \"elements\":[ {\"type\":\"mrkdwn\", \"text\": \":white_check_mark: $(MESSAGE)\n.\" } ] } ] }' $(SLACK_URL)"
 
-notify-failure: ## Sends notification on failure, MESSAGE and SLACK_URL have to be set
-	@echo "curl -s -X POST -H 'Content-type: application/json' -d '{\"blocks\":[ {\"type\":\"header\", \"text\":{ \"type\":\"plain_text\", \"text\": \"Task Failure\" }}, {\"type\":\"divider\"}, {\"type\":\"context\", \"elements\":[ {\"type\":\"mrkdwn\", \"text\": \":x: $(MESSAGE)\n.\" } ] } ] }' $(SLACK_URL)"
+notify-failure: ## Sends notification on failure, TITLE, MESSAGE and SLACK_URL have to be set
+	@echo "curl -s -X POST -H 'Content-type: application/json' -d '{\"blocks\":[ {\"type\":\"header\", \"text\":{ \"type\":\"plain_text\", \"text\": \"$(TITLE)\" }}, {\"type\":\"divider\"}, {\"type\":\"context\", \"elements\":[ {\"type\":\"mrkdwn\", \"text\": \":x: $(MESSAGE)\n.\" } ] } ] }' $(SLACK_URL)"
